24c06be272321ae1fae5920e512f4f639a0b62c0
aroesec <aroe3sec@gmail.com>
2022-08-24 20:41:05 -0600

    functions and loops

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -3,9 +3,9 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("The Secret Number is {secret_number}");
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
     loop {
         println!("Please input your guess");
 
@@ -19,6 +19,7 @@ fn main() {
             Err(_) => continue,
         };
         println!("You guessed {guess}");
+
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too Small"),
             Ordering::Greater => println!("Too Big"),
@@ -27,7 +28,5 @@ fn main() {
                 break;
             }
         }
-
-        println!("You guessed: {}", guess);
     }
 }
